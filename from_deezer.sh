a6bc8219881836d2c2f68965c8713a2cb46f4e9c
Flynn Duniho <flynn@harveyosity.org>
2026-04-23 13:56:15 -0700

    Set the file rawe to the trark title

--- from_deezer.sh
+++ from_deezer.sh
@@ -12,7 +12,9 @@ for file in "$in"/**/*.mp3; do
 		read -r album
 		read -r band
 		read -r partofset
-	} < <(exiftool -s3 -artist -album -band -partofset "$file")
+		read -r track
+		read -r title
+	} < <(exiftool -s3 -artist -album -band -partofset -track -title "$file")
 	band=${band:-$artist}
 	album=${album/\//}
 	if [[ "$partofset" =~ .*/.* ]] && [[ ! "$partofset" =~ .*/1 ]]; then
@@ -25,7 +27,6 @@ for file in "$in"/**/*.mp3; do
 	if [ ! -d "$folder" ]; then
 		mkdir -p "$folder"
 	fi
-	name="$(basename "$file")"
-	name="${name#*@}"
+  name="$(printf "%02d" ${track%\/*}). $title.mp3"
 	mv "$file" "$folder/$name"
 done
